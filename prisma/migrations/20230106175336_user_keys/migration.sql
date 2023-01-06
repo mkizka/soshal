c5f4645015cfbe9ef9683e179e840195d644ceb1
mkizka <g.mkizka@gmail.com>
2023-01-07 03:21:07 +0900

    key追加

---
+++ prisma/migrations/20230106175336_user_keys/migration.sql
@@ -0,0 +1,3 @@
+-- AlterTable
+ALTER TABLE "User" ADD COLUMN "privateKey" TEXT;
+ALTER TABLE "User" ADD COLUMN "publicKey" TEXT;
--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,3 +1,5 @@
+import crypto from "crypto";
+
 import NextAuth, { type NextAuthOptions } from "next-auth";
 import EmailProvider from "next-auth/providers/email";
 // Prisma adapter for NextAuth, optional and can be removed
@@ -9,11 +11,39 @@ import { prisma } from "../../../server/db";
 export const authOptions: NextAuthOptions = {
   // Include user.id on session
   callbacks: {
-    session({ session, user }) {
+    async session({ session, user }) {
       if (session.user) {
         session.user.id = user.id;
       }
       return session;
+    },
+  },
+  events: {
+    async createUser({ user }) {
+      // 以下のコマンドの内容と同じように生成する
+      //   openssl genrsa -out private.pem 2048
+      //   openssl rsa -in private.pem -outform PEM -pubout -out public.pem
+      // 参考: https://blog.joinmastodon.org/2018/06/how-to-implement-a-basic-activitypub-server/
+      const { privateKey, publicKey } = crypto.generateKeyPairSync("rsa", {
+        modulusLength: 2048,
+        publicKeyEncoding: {
+          type: "spki",
+          format: "pem",
+        },
+        privateKeyEncoding: {
+          type: "pkcs8",
+          format: "pem",
+        },
+      });
+      await prisma.user.update({
+        where: {
+          id: user.id,
+        },
+        data: {
+          publicKey,
+          privateKey,
+        },
+      });
     },
   },
   adapter: PrismaAdapter(prisma),
